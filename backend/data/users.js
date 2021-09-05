351a22a532d4c31014ea8fdaca4c2259a3040e1a
Med-Amine-Fredj <medamin.fredj@gmail.com>
2021-09-05 02:34:23 +0100

    Project Finished

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456',10),
+        isAdmin: true,
+    },
+    {
+        name: 'Fredj Med Amine',
+        email: 'fredj@example.com',
+        password: bcrypt.hashSync('123456',10), 
+    },
+    {
+        name: 'Friiidjii',
+        email: 'friiidjii@example.com',
+        password: bcrypt.hashSync('123456',10),
+    },
+]
+
+export default users
